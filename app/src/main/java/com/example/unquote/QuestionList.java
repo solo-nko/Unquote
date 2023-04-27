d6f7c4dde4b3ed007776c49b5f1a9abcbb6c72a1
nko <solowingrazgriz@outlook.com>
2023-04-27 10:56:55 +0900

    Add six new questions

--- app/src/main/java/com/example/unquote/QuestionList.java
+++ app/src/main/java/com/example/unquote/QuestionList.java
@@ -26,6 +26,27 @@ public class QuestionList
 		Question advice6 = new Question(R.drawable.img_quote_5, "Unfortunately, true - but did Marilyn Monroe convey it or did someone else?", "Laurel Thatcher Ulrich", "Eleanor Roosevelt", "Marilyn Monroe", "Queen Victoria", 0);
 		qList.add(advice6);
 
+		Question advice7 = new Question(R.drawable.img_quote_6, "Here's the truth, Will Smith DID say this, but in which movie?", "Independence Day", "Bad Boys", "Men in Black", "The Pursuit of Happiness", 2);
+		qList.add(advice7);
+
+		Question advice8 = new Question(R.drawable.img_quote_7, "Which TV funny gal actually quipped this 1-liner?", "Ellen Degeneres", "Amy Poehler", "Betty White", "Tina Fey", 3);
+		qList.add(advice8);
+
+		Question advice9 = new Question(R.drawable.img_quote_8, "This mayor won't get my vote - but did he actually give this piece of advice? And if not, who did?", "Forrest Gump", "Dorry (Finding Nemo)", "Esther Williams", "The Mayor (Jaws)", 1);
+		qList.add(advice9);
+
+		Question advice10 = new Question(R.drawable.img_quote_9, "Her heart will go on, but whose heart is it?", "Whitney Houston", "Diana Ross", "Celine Dion", "Mariah Carey", 0);
+		qList.add(advice10);
+
+		Question advice11 = new Question(R.drawable.img_quote_10, "He's the king of something alright - to whom does this self-titling line belong to?", "Tony Montana (Scarface)", "Joker (The Dark Knight)", "Lex Luthor (Batman v Superman)", "Jack (Titanic)", 3);
+		qList.add(advice11);
+
+		Question advice12 = new Question(R.drawable.img_quote_11, "Is \"Grey\" synonymous for \"wise\"? If so, maybe Gandalf did preach this advice. If not, who did?", "Yoda (Star Wars)", "Gandalf (Lord of the Rings)", "Dumbledore (Harry Potter)","Uncle Ben (Spider-Man)", 0);
+		qList.add(advice12);
+
+		Question advice13 = new Question(R.drawable.img_quote_12, "Houston, we have a problem with this quote - which space-traveler does this catchphrase actually belong to?", "Han Solo (Star Wars)", "Captain Kirk (Star Trek)", "Jim Lovell (Apollo 13)", "Buzz Lightyear (Toy Story)", 2);
+		qList.add(advice13);
+
 
 
 		Question eiffel = new Question(921238, "How tall is the Eiffel Tower?", "1024ft", "1063ft", "1124ft", "1163ft", 1);
