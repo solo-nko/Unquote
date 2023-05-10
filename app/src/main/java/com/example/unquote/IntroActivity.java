3b0d88a609f8138600efc20d967aecea54e045d2
nko <solowingrazgriz@outlook.com>
2023-05-10 21:18:04 +0900

    Add basic intro page and button to start game

--- app/src/main/java/com/example/unquote/IntroActivity.java
+++ app/src/main/java/com/example/unquote/IntroActivity.java
@@ -1,16 +1,30 @@
 package com.example.unquote;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class IntroActivity extends AppCompatActivity {
 
+	Button startButton;
+
 	@Override
 	protected void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.activity_intro);
+		startButton = findViewById(R.id.btn_gameStartBtn);
+
+		startButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(IntroActivity.this, MainActivity.class);
+				startActivity(intent);
+			}
+		});
 	}
 }
