3de7709780174d6c9fa156d4c34f4950830e5592
nko <solowingrazgriz@outlook.com>
2023-05-10 20:43:19 +0900

    Add new activity and java class

---
+++ app/src/main/java/com/example/unquote/IntroActivity.java
@@ -0,0 +1,16 @@
+package com.example.unquote;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class IntroActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.activity_intro);
+	}
+}
