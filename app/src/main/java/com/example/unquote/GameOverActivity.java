3cbce66811433b73d42d96f8dc7c47a1c67577ea
nko <solowingrazgriz@outlook.com>
2023-05-12 08:46:21 +0900

    Add gameover activity and results

--- app/src/main/java/com/example/unquote/GameOverActivity.java
+++ app/src/main/java/com/example/unquote/GameOverActivity.java
@@ -6,18 +6,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class GameOverActivity extends AppCompatActivity {
 	int totalCorrect = getIntent().getIntExtra("total_correct", 0);
+	double totalCorrectDouble = (double) totalCorrect;
 	int totalQuestions = getIntent().getIntExtra("total_questions", 0);
+	double totalQuestionsDouble = (double) totalQuestions;
+	double scorePercentage;
 	Button playAgainButton;
 	Button backToStartButton;
+	TextView scoreEvalText;
+	TextView scoreSummaryText;
+	TextView scorePercentText;
 
 	@Override
 	protected void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.activity_gameover);
+		scoreEvalText = findViewById(R.id.gameover_summary_evaltext);
+		scoreSummaryText = findViewById(R.id.gameover_summary_scoretext);
+		scorePercentText = findViewById(R.id.gameover_summary_scorepercent);
 		playAgainButton = findViewById(R.id.btn_replay);
 		backToStartButton = findViewById(R.id.btn_backToStart);
 
@@ -25,7 +35,21 @@ public class GameOverActivity extends AppCompatActivity {
 			@Override
 			public void onClick(View view) {
 				Intent introIntent = new Intent(GameOverActivity.this, IntroActivity.class);
+				startActivity(introIntent);
 			}
 		});
+
+		score();
+	}
+
+	public void score() {
+		scorePercentage = (totalCorrectDouble/totalQuestionsDouble)*100;
+		if (scorePercentage == 100) {
+			scoreEvalText.setText("You got all of them right!");
+		} else {
+			scoreEvalText.setText("Good game!");
+		}
+		scoreSummaryText.setText("You got " + totalCorrect + " out of " + totalQuestions + " right, for a final score of...");
+		scorePercentText.setText(scorePercentage + "%");
 	}
 }
